6081895b5a79bf0479884402cb60ae4d77223986
Kamil Tomczuk <s24802@pjwstk.edu.pl>
2021-10-06 16:58:05 +0200

    testowy commmit

--- src/00-hello-world.cpp
+++ src/00-hello-world.cpp
@@ -2,12 +2,10 @@
  * The very first program you should write in any new programming language.
  */
 
-
 #include <iostream>
 
 auto main() -> int
 {
-    std::cout << "Hello, World\n";
-    std::cout << "xxx";
+    std::cout << "Hello, Kamil Tomczuk\n";
     return 0;
 }
